83893d9ad51a887f3cfe2dbc2031d23ab7c173a8
Mingsicogi <codedoctor119@gmail.com>
2020-07-14 01:45:24 +0900

    + add jpa shard db setting configuration

---
+++ src/main/java/mins/study/db/app/common/constant/DbConstantCommon.java
@@ -0,0 +1,7 @@
+package mins.study.db.app.common.constant;
+
+public class DbConstantCommon {
+    public static final String commonDatasourceBeanName = "shard_datasource_";
+    public static final String commonJpaEntityManagerBeanName = "shard_entityManager_";
+    public static final String commonTransactionManagerBeanName = "shard_transactionManager_";
+}
