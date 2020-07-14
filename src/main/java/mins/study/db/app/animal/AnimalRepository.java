76f90d8b9395009c862afd1e3e2ac0734a9b381e
Mingsicogi <codedoctor119@gmail.com>
2020-07-15 00:24:11 +0900

    + add jpa shard db setting configuration

--- src/main/java/mins/study/db/app/animal/AnimalRepository.java
+++ src/main/java/mins/study/db/app/animal/AnimalRepository.java
@@ -1,8 +1,6 @@
 package mins.study.db.app.animal;
 
-import mins.study.db.app.animal.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
